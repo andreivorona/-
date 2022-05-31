115688e7f90ca0d9a09a6db83511ec54ee826500
andreivorona <andreivorona@abv.bg>
2022-05-31 12:26:54 +0300

    Add 2 questions

---
+++ BankRobber/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace BankRobber
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please add number of witnesses");
+
+            int numberOfPersons = int.Parse(Console.ReadLine());
+
+            List<string> namesOfwitnesses = new List<string>();
+
+            List<string> indicationsOfwitnesses = new List<string>();
+
+            List<string> notGuilty = new List<string>();
+          
+            for (int i = 0; i < numberOfPersons; i++)
+            {
+                Console.WriteLine("Please add name of witnes");
+
+                string nameOfwitnes = Console.ReadLine();
+
+                Console.WriteLine("Please add indication of witnes");
+
+                string indicationOfwitnes = Console.ReadLine();
+
+                namesOfwitnesses.Add(nameOfwitnes);
+
+                indicationsOfwitnesses.Add(indicationOfwitnes);
+            }
+
+            for (int i = 0; i < namesOfwitnesses.Count; i++)
+            {
+                for (int j = 0; j < indicationsOfwitnesses.Count; j++)
+                {
+                    if (indicationsOfwitnesses[j].Contains(namesOfwitnesses[i]))
+                    {
+                        notGuilty.Add(namesOfwitnesses[i]);
+
+                        notGuilty.Add(namesOfwitnesses[j]);
+                    }
+                }
+            }
+
+            foreach (string name in notGuilty)
+            {
+                namesOfwitnesses.Remove(name);
+            }
+
+            foreach (string name in namesOfwitnesses)
+            {
+                Console.WriteLine($"The guilty is: {name}");
+            }
+        }
+    }
+}
---
+++ FindPassword/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace FindPassword
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please type 3 digits on the each line 5 times");
+
+            int firstDigit = int.MinValue;
+            int secondDigit = int.MinValue;
+            int thirdDigit = int.MinValue;
+
+            for (int i = 0; i <= 4; i++)
+            {
+                char[] digitLine = Console.ReadLine().ToCharArray();
+                
+                //one digit is correct and is correct place
+                if (i == 0)
+                {
+                    thirdDigit = digitLine[2];
+                }
+
+                //one digit is correct and is incorrect place
+                if (i == 1)
+                {
+                    secondDigit = digitLine[2];
+                }
+
+                //two didits is correct and is incorrect places
+                if (i == 2)
+                {
+                    firstDigit = digitLine[1];
+                    thirdDigit = digitLine[0];
+                }
+
+                //all is incorrect
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                //one digit is correct and is incorrect place
+                if (i == 4)
+                {
+                    firstDigit = digitLine[2];
+                }
+            }
+
+            Console.WriteLine($"Password is: {(char)firstDigit}{(char)secondDigit}{(char)thirdDigit}");
+        }
+    }
+}
